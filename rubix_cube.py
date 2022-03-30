1378f49b9b9774761cdbb17cb3742400c2b2bb07
Errow1241 <59068471+Errow1241@users.noreply.github.com>
2022-03-30 10:38:50 -0500

    Changed array names.

--- rubix_cube.py
+++ rubix_cube.py
@@ -1,26 +1,26 @@
 import random
 
-ary = ['Red', 'Green', 'Blue']
+colors = ['Red', 'Green', 'Blue']
 
-color = random.choice(ary)
-color1 = random.choice(ary)
-color2 = random.choice(ary)
-color3 = random.choice(ary)
-color4 = random.choice(ary)
-color5 = random.choice(ary)
-color6 = random.choice(ary)
-color7 = random.choice(ary)
-color8 = random.choice(ary)
+color = random.choice(colors)
+color1 = random.choice(colors)
+color2 = random.choice(colors)
+color3 = random.choice(colors)
+color4 = random.choice(colors)
+color5 = random.choice(colors)
+color6 = random.choice(colors)
+color7 = random.choice(colors)
+color8 = random.choice(colors)
 
 red = ['Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', ]
 green = ['Green', 'Green', 'Green', 'Green', 'Green', 'Green', 'Green', 'Green', 'Green', ]
 blue = ['Blue', 'Blue', 'Blue', 'Blue', 'Blue', 'Blue', 'Blue', 'Blue', 'Blue', ]
 
-cary = [color,color1,color2,color3,color4,color5,color6,color7,color8]
+color_array = [color,color1,color2,color3,color4,color5,color6,color7,color8]
 
 print("%s|%s|%s" % (color,color1,color2))
 print("%s|%s|%s" % (color3,color4,color5))
 print("%s|%s|%s" % (color6,color7,color8))
 
-if cary == green or cary == red or cary == blue:
+if color_array == green or cary == red or cary == blue:
     print('You Win')
